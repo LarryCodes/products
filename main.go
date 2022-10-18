ccffbc59792a7857085c02485017e6da96733e64
larrycodes <larryokongo@icloud.com>
2022-10-18 23:59:49 +0300

    Initial server implementation.

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/LarryCodes/products/handlers"
+)
+
 func main() {
+
+	productsHandler := handlers.NewProductsHandler()
+
+	mux := http.NewServeMux()
+	mux.Handle("/", productsHandler)
+
+	server := &http.Server{
+		Addr:         ":9000",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+		IdleTimeout:  time.Second * 120,
+	}
+
+	log.Fatalln(server.ListenAndServe())
 
 }
