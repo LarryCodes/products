816ccab62eec0b048980033d0d2851a13b9e5a32
larrycodes <larryokongo@icloud.com>
2022-10-19 23:03:12 +0300

    Adding products

--- handlers/products.go
+++ handlers/products.go
@@ -16,10 +16,35 @@ func NewProductsHandler(l *log.Logger) *ProductsHandler {
 }
 
 func (h *ProductsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+		h.getProducts(w)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		h.addProduct(w, r)
+		return
+	}
+
+}
+
+func (p *ProductsHandler) getProducts(w http.ResponseWriter) {
 	productsList := models.GetProducts()
 	err := productsList.ToJson(w)
 	if err != nil {
 		http.Error(w, "Unable to encode json", http.StatusBadRequest)
 	}
+}
 
+func (p *ProductsHandler) addProduct(w http.ResponseWriter, r *http.Request) {
+
+	data := &models.Product{}
+	err := data.FromJson(r)
+	if err != nil {
+		p.l.Println(err)
+		http.Error(w, "Unable to decode json object", http.StatusBadRequest)
+	}
+
+	models.AddProduct(data)
 }
--- models/products.go
+++ models/products.go
@@ -3,24 +3,25 @@ package models
 import (
 	"encoding/json"
 	"io"
+	"net/http"
 	"time"
 )
 
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float64
-	Sku         string
-	CreatedAt   string
-	UpdatedAt   string
-	DeletedAt   string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Sku         string  `json:"sku"`
+	CreatedAt   string  `json:"-"`
+	UpdatedAt   string  `json:"-"`
+	DeletedAt   string  `json:"-"`
 }
 
 type Products []*Product
 
 var productsList = []*Product{
-	&Product{
+	{
 		ID:          1,
 		Name:        "Latte",
 		Description: "From the milky coffee",
@@ -29,7 +30,7 @@ var productsList = []*Product{
 		CreatedAt:   time.Now().UTC().String(),
 		UpdatedAt:   time.Now().UTC().String(),
 	},
-	&Product{
+	{
 		ID:          2,
 		Name:        "Esspresso",
 		Description: "Short and strong without coffee",
@@ -40,6 +41,18 @@ var productsList = []*Product{
 	},
 }
 
+func (p *Product) FromJson(r *http.Request) error {
+	decoder := json.NewDecoder(r.Body)
+	return decoder.Decode(p)
+}
+
+func AddProduct(p *Product) {
+	// First add the correct id
+	p.ID = getNextId()
+	//Append the product to the products list slice
+	productsList = append(productsList, p)
+}
+
 func GetProducts() Products {
 	return productsList
 }
@@ -47,4 +60,10 @@ func GetProducts() Products {
 func (p *Products) ToJson(w io.Writer) error {
 	encoder := json.NewEncoder(w)
 	return encoder.Encode(p)
+}
+
+func getNextId() int {
+	lastIdx := productsList[len(productsList)-1]
+
+	return lastIdx.ID + 1
 }
